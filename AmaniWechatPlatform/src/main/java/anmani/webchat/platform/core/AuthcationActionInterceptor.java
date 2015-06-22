ab6e5eaee64b091f160d301769f0a758ab413137
vivlanwong <vivlanwong120729@gmail.com>
2015-06-23 01:15:04 +0800

    添加Hibernate和log4引用

--- AmaniWechatPlatform/src/main/java/anmani/webchat/platform/core/AuthcationActionInterceptor.java
+++ AmaniWechatPlatform/src/main/java/anmani/webchat/platform/core/AuthcationActionInterceptor.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
  
-/**
+/** 
  * 身份验证拦截器
  * 
  * @author VV 
